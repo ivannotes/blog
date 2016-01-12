c8fc9777ad0352e2dcb570ae61c975f437a53fc6
ivan <miracle.ivanlee@gmail.com>
2016-01-12 23:50:09 +0800

    Configure static settings

--- blog/settings.py
+++ blog/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/tmp/blog/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
